284436af59645e1434e047febc1ab174bf579b48
裴龙云 <357551227@qq.com>
2022-04-17 16:04:15 +0800

    token verify

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,3 +1,4 @@
+import hashlib
 from datetime import datetime
 from flask import render_template, request
 from run import app
@@ -5,6 +6,34 @@ from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter
 from wxcloudrun.model import Counters
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
 
+
+@app.route('/wechat')
+def wechat():
+
+    # 1、 获取携带的 signature、timestamp、nonce、echostr
+    signature = request.args.get("signature", "")
+    timestamp = request.args.get("timestamp", "")
+    nonce = request.args.get("nonce", "")
+    echostr = request.args.get("echostr", "")
+    print(signature, timestamp, nonce, echostr)
+
+    token = "clown1988"
+
+    # 2、 进行字典排序
+    data = [token, timestamp, nonce]
+    data.sort()
+
+    # 3、三个参数拼接成一个字符串并进行sha1加密
+    temp = ''.join(data)
+    sha1 = hashlib.sha1(temp.encode('utf-8'))
+    hashcode = sha1.hexdigest()
+    print(hashcode)
+
+    # 4、对比获取到的signature与根据上面token生成的hashcode，如果一致，则返回echostr，对接成功
+    if hashcode == signature:
+        return echostr
+    else:
+        return "error"
 
 @app.route('/')
 def index():
